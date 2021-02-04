e104f29fd753e2e3ff360c1e58f451410f765576
Marc M <201810695@post.au.dk>
2021-02-04 13:46:45 +0100

    Added Accumulator that returns last result.

--- SWT_0101_Calculator/Calculator.cs
+++ SWT_0101_Calculator/Calculator.cs
@@ -4,28 +4,38 @@ namespace SWT_0101_Calculator
 {
     public class Calculator
     {
+        public double Accumulator { get; private set; }
+
         static void Main(string[] args)
         {
             
         }
         public double Add(double a, double b)
         {
-            return a + b;
+            var result = a + b;
+            Accumulator = result;
+            return result;
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            var result = a - b;
+            Accumulator = result;
+            return result;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            var result = a * b;
+            Accumulator = result;
+            return result;
         }
 
-        public  double Power(double a, double b)
+        public double Power(double a, double exp)
         {
-            return Math.Pow(a, b);
+            var result = Math.Pow(a,exp);
+            Accumulator = result;
+            return result;
         }
     }
 }
