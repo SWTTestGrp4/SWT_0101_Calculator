591ab1f666967938c77694f64b69571da480c2c5
Sabrowna <60734481+Sabrowna@users.noreply.github.com>
2021-02-04 14:04:12 +0100

    ClearTest added

--- Calculator.Unit.Test/Tests.cs
+++ Calculator.Unit.Test/Tests.cs
@@ -5,6 +5,7 @@ namespace SWT_0101_Calculator
     public class Tests
     {
         private Calculator uut;
+
         [SetUp]
         public void Setup()
         {
@@ -16,28 +17,37 @@ namespace SWT_0101_Calculator
         public void Add_2add2_returns4()
         {
             Calculator uut = new Calculator();
-            Assert.That(uut.Add(2,2), Is.EqualTo(4));
+            Assert.That(uut.Add(2, 2), Is.EqualTo(4));
         }
 
         [Test]
         public void Subtract_4minus2_returns2()
         {
             Calculator uut = new Calculator();
-            Assert.That(uut.Subtract(4,2), Is.EqualTo(2));
+            Assert.That(uut.Subtract(4, 2), Is.EqualTo(2));
         }
 
         [Test]
         public void Multiply_2x2_returns4()
         {
             Calculator uut = new Calculator();
-            Assert.That(uut.Multiply(2,2), Is.EqualTo(4));
+            Assert.That(uut.Multiply(2, 2), Is.EqualTo(4));
         }
 
         [Test]
         public void Power_2pow2_returns8()
         {
             Calculator uut = new Calculator();
-            Assert.That(uut.Add(2,2), Is.EqualTo(4));
+            Assert.That(uut.Add(2, 2), Is.EqualTo(4));
+        }
+
+        [Test]
+
+        public void Clear_clearAccumulator_AccumCleared()
+        {
+            uut.Add(5, 10);
+            uut.Clear();
+            Assert.That(uut.Accumulator, Is.EqualTo(0));
         }
     }
 }
